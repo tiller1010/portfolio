d89ccd1ecfb209e6090bd3dfa68754d4cbabaf46
Tyler Trout <tiller1010@gmail.com>
2018-10-22 13:02:59 -0400

    Updates

---
+++ precache-manifest.3895a953c8adb6042598b82a679e0ef0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aa52c9a85015972b1481",
+    "url": "/static/js/main.aa52c9a8.chunk.js"
+  },
+  {
+    "revision": "d1deddf27b1ac9a914f8",
+    "url": "/static/js/1.d1deddf2.chunk.js"
+  },
+  {
+    "revision": "aa52c9a85015972b1481",
+    "url": "/static/css/main.386bd040.chunk.css"
+  },
+  {
+    "revision": "133e30988c04665fefbd81846b45e13e",
+    "url": "/index.html"
+  }
+];

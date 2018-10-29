fd647ae473bb30f2827be0edf2eedc5ddbb6c0f4
Tyler Trout <tiller1010@gmail.com>
2018-10-29 18:15:16 -0400

    responsive layout

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ class Window extends Component{
   render() {
     if(this.props.show==='Home'){
       return (
-        <div>
+        <div className='window'>
           <Description/>
         </div>
       );
     }
     if(this.props.show==='Projects'){
       return(
-        <div>
+        <div className='window'>
           <h3>My knife project is a interactive store page with a visual shopping cart. The app was made in an object oriented style. Each knife inherits the jQuery slide toggled description.
           <Projects name='Knife Store' link='https://tiller1010.github.io/My-Knife-Project/'/></h3>
           <h3>The family tree project also utilizes an object oriented style, but this project allows for more objects to be created by the user. The user may even insert the url of an image to add their own person to the tree. This project also includes a feature that uses regular expressions to search for all objects that match the given properies.
@@ -26,7 +26,7 @@ class Window extends Component{
     }
     if(this.props.show==='Links'){
       return(
-        <div>
+        <div className='window'>
           <Link source='https://image.flaticon.com/icons/png/512/25/25231.png' alternative='GitHub' destination='https://github.com/tiller1010'/>
           <Link source='https://cdn-images-1.medium.com/max/1200/1*QNimSWsBQxta_xt3XksQaw.png' alternative='SoloLearn' destination='https://www.sololearn.com/Profile/9677467'/>
           <Link source='http://assets.stickpng.com/thumbs/5847f91ccef1014c0b5e48b8.png' alternative='freeCodeCamp' destination='https://www.freecodecamp.org/tiller1010'/>
@@ -35,7 +35,7 @@ class Window extends Component{
     }
     if(this.props.show==='Contact'){
       return(
-        <h3>
+        <h3 className='window'>
           Email: tiller1010@gmail.com
         </h3>
       );

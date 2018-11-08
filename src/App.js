33f30148686f7b05ab254f9b4e37c02ff3997249
Tyler Trout <tiller1010@gmail.com>
2018-11-08 16:48:45 -0500

    some working routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import Description from './aboutMe.js';
 import Projects from './projectList.js';
@@ -8,30 +9,39 @@ import MyFace from './MyFace.jpg';
 
 class Window extends Component{
   render() {
-    if(this.props.show==='Home'){
-      return (
-        <div className='window'>
-          <Description/>
-        </div>
-      );
-    }
-    if(this.props.show==='Projects'){
-      return(
-        <div className='window'>
-          <h3>My knife project is a interactive store page with a visual shopping cart. The app was made in an object oriented style. Each knife inherits the jQuery slide toggled description.
-          <Projects name='Knife Store' link='https://tiller1010.github.io/My-Knife-Project/'/></h3>
-          <h3>The family tree project also utilizes an object oriented style, but this project allows for more objects to be created by the user. The user may even insert the url of an image to add their own person to the tree. This project also includes a feature that uses regular expressions to search for all objects that match the given properies.
-            <Projects name= 'Family Tree' link='https://tiller1010.github.io/familyTree'/></h3>
-        </div>
-      );
-    }
-    if(this.props.show==='Contact'){
-      return(
-        <h3 className='window'>
-          Email: tiller1010@gmail.com
-        </h3>
-      );
-    }
+    return(
+      <main>
+        <Switch>
+          <Route exact path='/' component={Description}/>
+          <Route path='projects' component={Projects}/>
+        </Switch>
+      </main>
+    );
+
+    // if(this.props.show==='Home'){
+    //   return (
+    //     <div className='window'>
+    //       <Description/>
+    //     </div>
+    //   );
+    // }
+    // if(this.props.show==='Projects'){
+    //   return(
+    //     <div className='window'>
+    //       <h3>My knife project is a interactive store page with a visual shopping cart. The app was made in an object oriented style. Each knife inherits the jQuery slide toggled description.
+    //       <Projects name='Knife Store' link='https://tiller1010.github.io/My-Knife-Project/'/></h3>
+    //       <h3>The family tree project also utilizes an object oriented style, but this project allows for more objects to be created by the user. The user may even insert the url of an image to add their own person to the tree. This project also includes a feature that uses regular expressions to search for all objects that match the given properies.
+    //         <Projects name= 'Family Tree' link='https://tiller1010.github.io/familyTree'/></h3>
+    //     </div>
+    //   );
+    // }
+    // if(this.props.show==='Contact'){
+    //   return(
+    //     <h3 className='window'>
+    //       Email: tiller1010@gmail.com
+    //     </h3>
+    //   );
+    // }
   }
 }
 
@@ -61,8 +71,8 @@ class App extends Component {
         </div>
         <nav id='navBar'>
           <ul>
-            <li><Link to='/'>About</Link></li>
-            <li onClick={() => this.navigate('Projects')}>Projects</li>
+            <Link to='/'><li>About</li></Link>
+            <Link to='/projects'><li>Projects</li></Link>
             <li onClick={() => this.navigate('Contact')}>Contact</li>
           </ul>
         </nav>
--- src/projectList.js
+++ src/projectList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Projects extends Component{
+class ProjectLink extends Component{
   constructor(props){
     super(props);
     this.state={
@@ -23,6 +23,19 @@ class Projects extends Component{
         </a>
         Viewed: {this.state.viewed}
     </div>
+    );
+  }
+}
+
+class Projects extends Component{
+  render(){
+    return(
+      <div className='window'>
+        <h3>My knife project is a interactive store page with a visual shopping cart. The app was made in an object oriented style. Each knife inherits the jQuery slide toggled description.
+          <ProjectLink name='Knife Store' link='https://tiller1010.github.io/My-Knife-Project/'/></h3>
+        <h3>The family tree project also utilizes an object oriented style, but this project allows for more objects to be created by the user. The user may even insert the url of an image to add their own person to the tree. This project also includes a feature that uses regular expressions to search for all objects that match the given properies.
+          <ProjectLink name= 'Family Tree' link='https://tiller1010.github.io/familyTree'/></h3>
+      </div>
     );
   }
 }

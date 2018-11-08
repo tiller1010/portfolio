3451dfc2390dae5928b41fa307cf628f1d64c1b7
Tyler Trout <tiller1010@gmail.com>
2018-11-08 17:08:02 -0500

    Updates

---
+++ precache-manifest.d595ccd7241ead48e359230e5384ca15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "2aa85291f2ca770d0acd",
+    "url": "/portfolio/static/js/main.2aa85291.chunk.js"
+  },
+  {
+    "revision": "86dcf974072310339f7b",
+    "url": "/portfolio/static/js/1.86dcf974.chunk.js"
+  },
+  {
+    "revision": "2aa85291f2ca770d0acd",
+    "url": "/portfolio/static/css/main.6072b790.chunk.css"
+  },
+  {
+    "revision": "00b55b96179843ee34d15509b1e2ca87",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.79409327659fe98c97da27939343da61.js"
+  "/portfolio/precache-manifest.d595ccd7241ead48e359230e5384ca15.js"
 );
 
 workbox.clientsClaim();

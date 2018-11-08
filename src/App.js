f01b8d8b1142d263855a06145bf944b6f2d62d1c
Tyler Trout <tiller1010@gmail.com>
2018-11-08 17:05:51 -0500

    working routing

--- src/App.js
+++ src/App.js
@@ -13,53 +13,19 @@ class Window extends Component{
       <main>
         <Switch>
           <Route exact path='/' component={Description}/>
-          <Route path='projects' component={Projects}/>
+          <Route path='/projects' component={Projects}/>
+          <Route path='/contact' render={()=>
+            <h3 className='window'>
+                Email: tiller1010@gmail.com
+            </h3>
+          }/>
         </Switch>
       </main>
     );
-
-    // if(this.props.show==='Home'){
-    //   return (
-    //     <div className='window'>
-    //       <Description/>
-    //     </div>
-    //   );
-    // }
-    // if(this.props.show==='Projects'){
-    //   return(
-    //     <div className='window'>
-    //       <h3>My knife project is a interactive store page with a visual shopping cart. The app was made in an object oriented style. Each knife inherits the jQuery slide toggled description.
-    //       <Projects name='Knife Store' link='https://tiller1010.github.io/My-Knife-Project/'/></h3>
-    //       <h3>The family tree project also utilizes an object oriented style, but this project allows for more objects to be created by the user. The user may even insert the url of an image to add their own person to the tree. This project also includes a feature that uses regular expressions to search for all objects that match the given properies.
-    //         <Projects name= 'Family Tree' link='https://tiller1010.github.io/familyTree'/></h3>
-    //     </div>
-    //   );
-    // }
-    // if(this.props.show==='Contact'){
-    //   return(
-    //     <h3 className='window'>
-    //       Email: tiller1010@gmail.com
-    //     </h3>
-    //   );
-    // }
   }
 }
 
 class App extends Component {
-  constructor(props){
-    super();
-    this.state={
-      show:'Home'
-    }
-    this.navigate=this.navigate.bind(this);
-  }
-
-  navigate(page){
-    this.setState({
-      show: page
-    });
-  }
-
   render(){
     return(
       <div>
@@ -73,10 +39,10 @@ class App extends Component {
           <ul>
             <Link to='/'><li>About</li></Link>
             <Link to='/projects'><li>Projects</li></Link>
-            <li onClick={() => this.navigate('Contact')}>Contact</li>
+            <Link to='/contact'><li>Contact</li></Link>
           </ul>
         </nav>
-        <Window show={this.state.show}/>
+        <Window/>
       </div>
     );
   }

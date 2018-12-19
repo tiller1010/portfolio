b65e03af51ab421b6370f72676a6a5fa6491a4e0
Tyler Trout <tiller1010@gmail.com>
2018-12-19 11:11:00 -0500

    finished styling

--- src/contacts.js
+++ src/contacts.js
@@ -4,10 +4,10 @@ class Contacts extends Component{
   render(){
     return(
       <h3 className='window' id='contacts'>
-          <span className='contact' id='email'>Email:</span>tiller1010@gmail.com<br/>
-          <span className='contact' id='meetup'>Meetup:</span> ...<br/>
-          <span className='contact' id='upwork'>Upwork:</span> ....<br/>
-          <span className='contact' id='linkedIn'>LinkedIn:</span> ...
+          <div id='emailBox'><span className='contact' id='email'>Email:</span>tiller1010@gmail.com<br/></div>
+          <span className='contact' id='meetup'>Meetup:</span><a target='_blank' rel="noopener noreferrer" href='https://www.meetup.com/members/261337114/'>https://www.meetup.com/members/261337114/</a><br/>
+          <span className='contact' id='upwork'>Upwork:</span><a target='_blank' rel="noopener noreferrer" href='https://www.upwork.com/freelancers/~01e2adada235766c48'>https://www.upwork.com/freelancers/~01e2adada235766c48</a>.<br/>
+          <span className='contact' id='linkedIn'>LinkedIn:</span><a target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/tyler-trout-453b92173/'>https://www.linkedin.com/in/tyler-trout-453b92173/</a>
       </h3>
     );
   }

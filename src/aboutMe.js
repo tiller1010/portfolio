3714bea698daa76ff46ba73c96a88517af46ef8a
Tyler <tiller1010@gmail.com>
2019-03-07 14:50:03 -0500

    adding animations

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -10,14 +10,14 @@ const bookSeperatorStlye={
   letterSpacing: '4px',
   boxShadow:'3px 3px 0 #ddd, 6px 6px 0 #ccc, 9px 9px 0 #bbb, 12px 12px 0 #aaa',
   padding:'10px 0',
-  transform:'translateY(50px)'
+  transform:'translateY(40px)'
 }
 
 const Book=(props)=>{
   return(
     <div className='window' style={{marginTop: '10px'}}>
       <h3 className={props.side + 'Side'}>
-        <img src={props.image} alt={props.alt} style={{float: props.side, height:'200px', width:'160px', margin:'10px'}}/>
+        <img src={props.image} alt='book' style={{float: props.side, height:'200px', width:'160px', margin:'10px'}}/>
         <header>{props.title}</header><hr/>
         <p>{props.description}</p>
       </h3>
@@ -26,9 +26,15 @@ const Book=(props)=>{
 }
 
 class Description extends Component{
+
+  componentDidMount(){
+    let frames = document.querySelectorAll('.window');
+    frames[0].classList.add('animated');
+  }
+
   render(){
     return(
-      <div>
+      <div className='animatedContainer'>
         <h3 className='window'>
           <header>Introduction</header><hr/>
           <p>
@@ -53,6 +59,7 @@ class Description extends Component{
             I love to learn new things and do so every day. As of the beginning of this year, I have learned how to set up my own WordPress sites as well as the basics of developing themes from scratch. Currently, I have been learning some backend technologies such as PHP, SQL, and Node.js. My hope is to one day have the abilities to understand and work on any part of a web application and deliver an exceptional user experience. Coding has always and intrigued and delighted me, and I am excited for what I might create in the future!
           </p>
         </h3>
+        <hr style={{transform:'translateY(50px)'}}/>
         <h1 id='bookSeperator' style={bookSeperatorStlye}>Books I have read:</h1>
         <Book side='left' image='https://images-na.ssl-images-amazon.com/images/I/51lu4ArIFYL._SX258_BO1,204,203,200_.jpg' title='JavaScript The Definitive Guide' description='My first coding book, JavaScript The Definitive Guide felt almost like reading a code dictionary. Useful feature or not, this book covers what seems like everything at first. This book breaks down the language itself as well as the tools that can be used on the client side. This was a satisfying book to finish and really opened up my eyes to the scope of the language.'/>
         <Book side='right' image='https://images-na.ssl-images-amazon.com/images/I/51FHuacxYjL._SX379_BO1,204,203,200_.jpg' title='Learning React' description='React seemed dificult for me to learn at first. I was used to keeping HTML and JS seperated, but now I was supposed to marry the two in JSX? Learning React made React simple to understand and use. The book walks the reader through a few projects, teaching concepts by simply adding new features. React has become my favorite library and is what I used to build this portfolio.'/>

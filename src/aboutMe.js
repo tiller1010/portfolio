8fba2a146bbd1dd939a930ce889cb68da8c35fdb
Tyler <tiller1010@gmail.com>
2019-03-06 23:19:22 -0500

    updated about page

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -3,8 +3,32 @@ import React, { Component } from 'react';
 class Description extends Component{
   render(){
     return(
-      <h3 className='window'>My name is Tyler Trout. I am a self-taught aspiring web developer. I am looking for my first oppritunity at employment. I have been teaching myself to code for over eight months now. I understand the fundamentals of HTML, CSS and JavaScript. I can code in an object oriented and functional style. I can use Javascript libraries such as jQuery and React/Redux. I use technologies such as Git/GitHub, Chrome Dev Tools to assist myself in development.
-      I have a basic understanding of other technologies such as WordPress, PHP, and SQL. Code has always and intrigued me and I am excited for what I might create in the future!</h3>
+      <div>
+        <h3 className='window'>
+          <header>Introduction</header><hr/>
+          <p>
+            Looking for a new web developer that is easy to get along with and is always hungry to learn more? My name is Tyler Trout, and I am a self-taught aspiring web developer. I am looking to achieve my first employment oppritunity.
+          </p>
+        </h3>
+        <h3 className='window rightSide'>
+          <header>Experience</header><hr/>
+          <p>
+            I have over nine months of experience reading excellent coding books, completing challenges provided by various sources, and writing code for my own personal projects. I attend monthly meetups with Coderie and learn from the best!
+          </p>
+        </h3>
+        <h3 className='window leftSide'>
+          <header>Skills</header><hr/>
+          <p>
+            I have skills in HTML, CSS, JavaScript, and more! My framework of choice is React, but I also have the abilities to contain application state in Redux, as well as utilize the tools provided by jQuery. I use several various technologies to assist myself in development. I use Git and GitHub frequently to keep track of changes and upload my work. I use Chrome Dev Tools to inspect the contents of my application and read from the console.
+          </p>
+        </h3>
+        <h3 className='window rightSide'>
+          <header>Moving forward</header><hr/>
+          <p>
+            I love to learn new things and do so every day. As of the beginning of this year, I have learned how to set up my own WordPress sites as well as the basics of developing themes from scratch. Currently, I have been learning some backend technologies such as PHP, SQL, and Node.js. My hope is to one day have the abilities to understand and work on any part of a web application and deliver an exceptional user experience. Coding has always and intrigued and delighted me, and I am excited for what I might create in the future!
+          </p>
+        </h3>
+      </div>
     );
   }
 }

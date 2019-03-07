a0631e11800fe47dd4738053bf93bcef2b63ef3d
Tyler <tiller1010@gmail.com>
2019-03-07 04:31:47 -0500

    contact page updated

--- src/contacts.js
+++ src/contacts.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
+const Contact=(props)=>{
+  return(
+    <a target='_blank' href={props.link}>
+      <img src={props.image} style={{width: props.width, height: props.height, marginTop: props.verticalMargin}}/><br/>
+    </a>
+  )
+}
+
 class Contacts extends Component{
   render(){
     return(
       <h3 className='window' id='contacts'>
-          <div id='emailBox'><span className='contact' id='email'>Email:</span>tiller1010@gmail.com<br/></div>
-          <span className='contact' id='meetup'>Meetup:</span><a target='_blank' rel="noopener noreferrer" href='https://www.meetup.com/members/261337114/'>https://www.meetup.com/members/261337114/</a><br/>
-          <span className='contact' id='upwork'>Upwork:</span><a target='_blank' rel="noopener noreferrer" href='https://www.upwork.com/freelancers/~01e2adada235766c48'>https://www.upwork.com/freelancers/~01e2adada235766c48</a>.<br/>
-          <span className='contact' id='linkedIn'>LinkedIn:</span><a target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/tyler-trout-453b92173/'>https://www.linkedin.com/in/tyler-trout-453b92173/</a>
+      {
+      //     <div id='emailBox'><span className='contact' id='email'>Email:</span>tiller1010@gmail.com<br/></div>
+      //     <span className='contact' id='meetup'>Meetup:</span><a target='_blank' rel="noopener noreferrer" href='https://www.meetup.com/members/261337114/'>https://www.meetup.com/members/261337114/</a><br/>
+      //     <span className='contact' id='upwork'>Upwork:</span><a target='_blank' rel="noopener noreferrer" href='https://www.upwork.com/freelancers/~01e2adada235766c48'>https://www.upwork.com/freelancers/~01e2adada235766c48</a>.<br/>
+      //     <span className='contact' id='linkedIn'>LinkedIn:</span><a target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/tyler-trout-453b92173/'>https://www.linkedin.com/in/tyler-trout-453b92173/</a>
+      }
+      <a href='mailto:tiller1010@gmail.com'><img src='https://files.readme.io/3aa137c-logo_lockup_gmail_icon_horizontal.png' style={{width:'400px', height:'220px', display:'block', margin:'auto'}}/></a><hr/>
+      <Contact verticalMargin='20px' width='300px' height='100px' link='https://www.meetup.com/members/261337114/' image='https://secure.meetup.com/s/img/0/logo/svg/logo--script.svg'/>
+      <Contact verticalMargin='20px' width='300px' height='80px' link='https://www.linkedin.com/in/tyler-trout-453b92173/' image='https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1280px-LinkedIn_Logo.svg.png'/>
+      <Contact verticalMargin='35px' width='300px' height='160px' link='https://www.upwork.com/freelancers/~01e2adada235766c48' image='https://www.chrisjacobie.com/wp-content/uploads/2018/03/upwork2-300x169.png'/>
       </h3>
     );
   }
--- src/projectList.js
+++ src/projectList.js
@@ -31,16 +31,16 @@ class Projects extends Component{
   render(){
     return(
       <div className='window'>
-        <h3 className='projectFrame'>Generic Store is an interactive store page with a visual shopping cart. The app was made in an object oriented style. Each object contains a title, a brief summary, and a price. The objects are stored in a shopping cart in the form of an array. That array is stored in session storage, where it is parsed by JSON when the user navigates to the checkout page.
+        <h3 className='projectFrame'>Generic Store is an interactive store page with a visual shopping cart indicator. The app was made in an object oriented style. Each object contains a title, a brief summary, and a price. The objects are stored in a shopping cart in the form of an array. That array is stored in session storage, where it is parsed by JSON when the user navigates to the checkout page.
           <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/generic-store'><div className='codeBox'>Code</div></a>
           <ProjectLink name='Generic Store' link='https://tiller1010.github.io/generic-store/'/></h3>
-        <h3 className='projectFrame'>The family tree project also utilizes an object oriented style, but this project allows for more objects to be created by the user. The user may even insert the url of an image to add their own person to the tree. This project also includes a feature that uses regular expressions to search for all objects that match the given properies.
+        <h3 className='projectFrame'>The family tree project also utilizes an object oriented style, but this project allows for more objects to be created by the user. The user may even upload an image by file or URL. When clicked, a person box will display that person's properties, and a line will be drawn on the canvas, connecting child to parent. This project also includes a feature that uses regular expressions to search for all mothers and fathers.
           <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/familyTree'><div className='codeBox'>Code</div></a>
           <ProjectLink name= 'Family Tree' link='https://tiller1010.github.io/familyTree'/></h3>
-        <h3 className='projectFrame'>This website template includes multiple pages and a few neat features. The website is responsive and should change the appearance of the navigation bar. The website scales to fit on a mobile screen with a media query.
+        <h3 className='projectFrame'>This website template includes multiple pages and a few neat features such as an image slider, a contact form, and an animated menu on mobile devices. The website is responsive and should change the appearance of the navigation bar if viewed on a devices with smaller displays.
           <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/template1'><div className='codeBox'>Code</div></a>
           <ProjectLink name= 'Website Template' link='https://tiller1010.github.io/template1'/></h3>
-        <h3 className='projectFrame'>Redux vacation is a vacation planner that uses the React-Redux library. Redux is used to contain the state of the application in a single source of truth, tracking the booking status of the flight, destination, and hotel. The user interface will re-render everytime there is a change of state.
+        <h3 className='projectFrame'>Redux vacation is a vacation planner that uses the React-Redux library. Redux is used to contain the state of the application in a single source of truth. Redux tracks the booking status of the flight, destination, and hotel. The user interface will re-render everytime there is a change of state.
           <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/redux-vacation'><div className='codeBox'>Code</div></a>
           <ProjectLink name= 'Redux Vacation' link='https://tiller1010.github.io/redux-vacation'/></h3>
       </div>

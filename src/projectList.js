9ef4a98a0e6497f87cecbeb4b729046add3aa741
Tyler <tiller1010@gmail.com>
2019-06-23 15:35:37 -0400

    Project type swap works

--- src/projectList.js
+++ src/projectList.js
@@ -8,7 +8,9 @@ const projectHeadingStyle = {
   marginLeft: '40px',
   display: 'flex',
   justifyContent: 'space-between',
-  marginBottom: '10px'
+  alignItems: 'baseline',
+  marginBottom: '20px',
+  boxShadow: '0 4px 2px -2px #fff'
 }
 
 class ProjectLink extends Component{
@@ -39,6 +41,17 @@ class ProjectLink extends Component{
 }
 
 class ProjectHeading extends Component {
+  render(){
+    return(
+      <div style={projectHeadingStyle}>
+        <h2>{this.props.projectType}</h2>
+        <FontAwesomeIcon icon={faSyncAlt} style={{cursor: 'pointer', transform: 'scale(1.5)'}} onClick={this.props.switchType} />
+    </div>
+    )
+  }
+}
+
+class Projects extends Component{
   constructor(props){
     super(props);
     this.state = {
@@ -54,69 +67,75 @@ class ProjectHeading extends Component {
   }
 
   render(){
-    return(
-      <div style={projectHeadingStyle}>
-        <h2>{this.state.projectType}</h2>
-        <button style={{}} onClick={this.switchType}>Switch<FontAwesomeIcon icon={faSyncAlt} /></button>
-    </div>
-    )
-  }
-}
-
-class Projects extends Component{
-  render(){
-    return(
-      <div className='window animated' style={{visibility:'visible'}}>
-      <ProjectHeading/>
-        <h3 className='projectFrame'>
-          Generic Store is an interactive store page with a visual shopping cart
-          indicator. The app was made in an object-oriented style. Each object
-          contains a title, a brief summary, and a price. The objects are stored in a
-          shopping cart in the form of an array. That array is stored in session
-          storage, where it is parsed by JSON when the user navigates to the
-          checkout page.
-          <div style={{display: 'flex', justifyContent: 'flex-end', margin: '12px'}}>
-            <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/generic-store'><div className='codeBox'>Code</div></a>
-            <ProjectLink name='Generic Store' link='https://tiller1010.github.io/generic-store/'/>
+    if(this.state.projectType === 'Personal'){
+      return(
+        <div className='window animated' style={{visibility:'visible'}}>
+          <ProjectHeading projectType={this.state.projectType} switchType={this.switchType}/>
+          <h3 className='projectFrame'>
+            Generic Store is an interactive store page with a visual shopping cart
+            indicator. The app was made in an object-oriented style. Each object
+            contains a title, a brief summary, and a price. The objects are stored in a
+            shopping cart in the form of an array. That array is stored in session
+            storage, where it is parsed by JSON when the user navigates to the
+            checkout page.
+            <div style={{display: 'flex', justifyContent: 'flex-end', margin: '12px'}}>
+              <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/generic-store'><div className='codeBox'>Code</div></a>
+              <ProjectLink name='Generic Store' link='https://tiller1010.github.io/generic-store/'/>
+            </div>
+          </h3>
+          <h3 className='projectFrame'>
+            The family tree project also utilizes an object-oriented style, but
+            this project allows for more objects to be created by the user. The
+            user may even upload an image by file or URL. When clicked, a
+            person box will display that person&#39;s properties, and a line will be
+            drawn on the canvas, connecting child to parent. This project
+            also includes a feature that uses regular expressions to search
+            for all mothers and fathers.
+            <div style={{display: 'flex', justifyContent: 'flex-end', margin: '12px'}}>
+              <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/familyTree'><div className='codeBox'>Code</div></a>
+              <ProjectLink name= 'Family Tree' link='https://tiller1010.github.io/familyTree'/>
+            </div>
+          </h3>
+          <h3 className='projectFrame'>
+            This website template includes multiple pages and a few neat
+            features such as an image slider, a contact form, and a menu that
+            animates on mobile devices. The website is responsive and
+            changes the appearance of the navigation bar if viewed on
+            devices with smaller displays.
+            <div style={{display: 'flex', justifyContent: 'flex-end', margin: '12px'}}>
+              <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/template1'><div className='codeBox'>Code</div></a>
+              <ProjectLink name= 'Website Template' link='https://tiller1010.github.io/template1'/>
+            </div>
+          </h3>
+          <h3 className='projectFrame'>
+            Redux vacation is a vacation planner that uses the React-Redux
+            library. Redux is used to contain the state of the application in a
+            single source of truth. Redux tracks the booking status of the
+            flight, destination, and hotel. The user interface will re-render
+            every time there is a change of state.
+            <div style={{display: 'flex', justifyContent: 'flex-end', margin: '12px'}}>
+              <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/redux-vacation'><div className='codeBox'>Code</div></a>
+              <ProjectLink name= 'Redux Vacation' link='https://tiller1010.github.io/redux-vacation'/>
+            </div>
+          </h3>
+        </div>
+      );
+    }
+    else{
+        return(
+          <div className='window animated' style={{visibility:'visible'}}>
+            <ProjectHeading projectType={this.state.projectType} switchType={this.switchType}/>
+              <h3 className='projectFrame'>
+              Gears & Grub event in Erie. They needed a site that would list all bands, food trucks, and vendors.
+              They also needed to direct visitors to the location of the event, as well as the weather.
+              I built them a Wordpress site so that they could manage updates and posts as well as utilize a few plugins.
+              <div style={{display: 'flex', justifyContent: 'flex-end', margin: '12px'}}>
+                <ProjectLink name= 'Gears & Grub' link='https://gearsandgruberie.com'/>
+              </div>
+            </h3>
           </div>
-        </h3>
-        <h3 className='projectFrame'>
-          The family tree project also utilizes an object-oriented style, but
-          this project allows for more objects to be created by the user. The
-          user may even upload an image by file or URL. When clicked, a
-          person box will display that person&#39;s properties, and a line will be
-          drawn on the canvas, connecting child to parent. This project
-          also includes a feature that uses regular expressions to search
-          for all mothers and fathers.
-          <div style={{display: 'flex', justifyContent: 'flex-end', margin: '12px'}}>
-            <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/familyTree'><div className='codeBox'>Code</div></a>
-            <ProjectLink name= 'Family Tree' link='https://tiller1010.github.io/familyTree'/>
-          </div>
-        </h3>
-        <h3 className='projectFrame'>
-          This website template includes multiple pages and a few neat
-          features such as an image slider, a contact form, and a menu that
-          animates on mobile devices. The website is responsive and
-          changes the appearance of the navigation bar if viewed on
-          devices with smaller displays.
-          <div style={{display: 'flex', justifyContent: 'flex-end', margin: '12px'}}>
-            <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/template1'><div className='codeBox'>Code</div></a>
-            <ProjectLink name= 'Website Template' link='https://tiller1010.github.io/template1'/>
-          </div>
-        </h3>
-        <h3 className='projectFrame'>
-          Redux vacation is a vacation planner that uses the React-Redux
-          library. Redux is used to contain the state of the application in a
-          single source of truth. Redux tracks the booking status of the
-          flight, destination, and hotel. The user interface will re-render
-          every time there is a change of state.
-          <div style={{display: 'flex', justifyContent: 'flex-end', margin: '12px'}}>
-            <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/redux-vacation'><div className='codeBox'>Code</div></a>
-            <ProjectLink name= 'Redux Vacation' link='https://tiller1010.github.io/redux-vacation'/>
-          </div>
-        </h3>
-      </div>
-    );
+        )
+      }
   }
 }
 

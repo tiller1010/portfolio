d8a55c79ce5c219d48119250ef7836706a164c7c
Tyler <tiller1010@gmail.com>
2019-11-18 17:41:23 -0500

    Refactored projects page

--- src/projectPage.js
+++ src/projectPage.js
@@ -51,6 +51,18 @@ class ProjectLink extends Component{
   }
 }
 
+const ProjectFrame = (props) => {
+  return (
+    <div className='projectFrame box-container'>
+      {props.Description}
+      <div style={{display: 'flex', justifyContent: 'center', margin: '12px'}}>
+        {props.CodeLink ? <a target='_blank' rel="noopener noreferrer" href={props.CodeLink}><div className='codeBox'>Code</div></a> : ''}
+        <ProjectLink name={props.Name} link={props.UserLink}/>
+      </div>
+    </div>
+  );
+}
+
 class Projects extends Component{
   constructor(props){
     super(props);
@@ -71,72 +83,62 @@ class Projects extends Component{
       return(
         <div className='window animated' style={{visibility:'visible'}}>
           <ProjectHeading projectType={this.state.projectType} switchType={this.switchType}/>
-          <div className='projectFrame box-container'>
+          <ProjectFrame Description='
             Generic Store is an interactive store page with a visual shopping cart
             indicator. The app was made in an object-oriented style. Each object
             contains a title, a brief summary, and a price. The objects are stored in a
             shopping cart in the form of an array. That array is stored in session
             storage, where it is parsed by JSON when the user navigates to the
-            checkout page.
-            <div style={{display: 'flex', justifyContent: 'center', margin: '12px'}}>
-              <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/generic-store'><div className='codeBox'>Code</div></a>
-              <ProjectLink name='Generic Store' link='https://tiller1010.github.io/generic-store/'/>
-            </div>
-          </div>
-          <div className='projectFrame box-container'>
+            checkout page.'
+            Name='Generic Store'
+            CodeLink='https://github.com/tiller1010/generic-store'
+            UserLink='https://tiller1010.github.io/generic-store/'/>
+          <ProjectFrame Description='
             The family tree project also utilizes an object-oriented style, but
             this project allows for more objects to be created by the user. The
             user may even upload an image by file or URL. When clicked, a
             person box will display that person&#39;s properties, and a line will be
             drawn on the canvas, connecting child to parent. This project
             also includes a feature that uses regular expressions to search
-            for all mothers and fathers.
-            <div style={{display: 'flex', justifyContent: 'center', margin: '12px'}}>
-              <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/familyTree'><div className='codeBox'>Code</div></a>
-              <ProjectLink name= 'Family Tree' link='https://tiller1010.github.io/familyTree'/>
-            </div>
-          </div>
-          <div className='projectFrame box-container'>
+            for all mothers and fathers.'
+            Name='Family Tree'
+            CodeLink='https://github.com/tiller1010/familyTree'
+            UserLink='https://tiller1010.github.io/familyTree'/>
+          <ProjectFrame Description='
             This website template includes multiple pages and a few neat
             features such as an image slider, a contact form, and a menu that
             animates on mobile devices. The website is responsive and
             changes the appearance of the navigation bar if viewed on
-            devices with smaller displays.
-            <div style={{display: 'flex', justifyContent: 'center', margin: '12px'}}>
-              <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/template1'><div className='codeBox'>Code</div></a>
-              <ProjectLink name= 'Website Template' link='https://tiller1010.github.io/template1'/>
-            </div>
-          </div>
-          <div className='projectFrame box-container'>
+            devices with smaller displays.'
+            Name='Website Template'
+            CodeLink='https://github.com/tiller1010/template1'
+            UserLink='https://tiller1010.github.io/template1'/>
+          <ProjectFrame Description='
             Redux vacation is a vacation planner that uses the React-Redux
             library. Redux is used to contain the state of the application in a
             single source of truth. Redux tracks the booking status of the
             flight, destination, and hotel. The user interface will re-render
-            every time there is a change of state.
-            <div style={{display: 'flex', justifyContent: 'center', margin: '12px'}}>
-              <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/redux-vacation'><div className='codeBox'>Code</div></a>
-              <ProjectLink name= 'Redux Vacation' link='https://tiller1010.github.io/redux-vacation'/>
-            </div>
-          </div>
-          <div className='projectFrame box-container'>
-            This is my first app that uses backend technology. The hospital app is built with PHP and SQL.
-            After logging in or making an account, users can submit appointments with their doctor of choice. All of which is stored in a database.
-            Private doctor accounts can then view the appointments that were submitted to them by pulling from the database.
-            <div style={{display: 'flex', justifyContent: 'center', margin: '12px'}}>
-              <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/hospitalApp'><div className='codeBox'>Code</div></a>
-              <ProjectLink name= 'Hospital App' link='http://hospitalapp.esy.es'/>
-            </div>
-          </div>
-          <div className='projectFrame box-container'>
+            every time there is a change of state.'
+            Name='Redux Vacation'
+            CodeLink='https://github.com/tiller1010/redux-vacation'
+            UserLink='https://tiller1010.github.io/redux-vacation/'/>
+          <ProjectFrame Description='
+            This is my first app that uses backend technology.
+            The hospital app is built with PHP and SQL.
+            After logging in or making an account, users can submit appointments with their doctor of choice.
+            All of which is stored in a database.
+            Private doctor accounts can then view the appointments that were submitted to them by pulling from the database.'
+            Name='Hospital App'
+            CodeLink='https://github.com/tiller1010/hospitalApp'
+            UserLink='http://hospitalapp.esy.es'/>
+          <ProjectFrame Description="
             This project is a dog personality test. It is built with Node and Express.
             The site uses two APIs: one for finding the dogs' traits and another for googling an image of the dog.
             The traits are ranked based on the users answers and then compared to the API response.
-            As an added bonus, this project also uses Sass styles compiled with Grunt.
-            <div style={{display: 'flex', justifyContent: 'center', margin: '12px'}}>
-              <a target='_blank' rel="noopener noreferrer" href='https://github.com/tiller1010/dog-personality'><div className='codeBox'>Code</div></a>
-              <ProjectLink name= 'Dog Personality' link='http://dog-personality.herokuapp.com'/>
-            </div>
-          </div>
+            As an added bonus, this project also uses Sass styles compiled with Grunt."
+            Name='Dog Personality'
+            CodeLink='https://github.com/tiller1010/dog-personality'
+            UserLink='http://dog-personality.herokuapp.com'/>
         </div>
       );
     }
@@ -144,22 +146,18 @@ class Projects extends Component{
         return(
           <div className='window animated' style={{visibility:'visible'}}>
             <ProjectHeading projectType={this.state.projectType} switchType={this.switchType}/>
-              <div className='projectFrame box-container'>
-                 Siri Thai Truck asked for help customizing their Weebly page. 
-                 I got to learn a little bit about how Weebly works and added custom elements and styles.
-                 The page now includes new fonts, a slight gradient, and a site icon.
-                <div style={{display: 'flex', justifyContent: 'center', margin: '12px'}}>
-                  <ProjectLink name= 'Siri Thai Truck' link='http://sirithaitruck.com'/>
-                </div>
-              </div>
-              <div className='projectFrame box-container'>
-                The Gears & Grub event in Erie needed a site that would list all bands, food trucks, and vendors. 
-                They also needed to direct visitors to the location of the event, as well as the weather. 
-                I built them a Wordpress site so that they could manage updates and posts as well as utilize a few plugins.
-                <div style={{display: 'flex', justifyContent: 'center', margin: '12px'}}>
-                  <ProjectLink name= 'Gears & Grub' link='https://gearsandgruberie.com'/>
-                </div>
-              </div>
+            <ProjectFrame Description="
+              Siri Thai Truck asked for help customizing their Weebly page. 
+              I got to learn a little bit about how Weebly works and added custom elements and styles.
+              The page now includes new fonts, a slight gradient, and a site icon."
+              Name='Siri Thai Truck'
+              UserLink='http://sirithaitruck.com'/>
+            <ProjectFrame Description="
+              The Gears & Grub event in Erie needed a site that would list all bands, food trucks, and vendors. 
+              They also needed to direct visitors to the location of the event, as well as the weather. 
+              I built them a Wordpress site so that they could manage updates and posts as well as utilize a few plugins."
+              Name='Gears & Grub'
+              UserLink='https://gearsandgruberie.com'/>
           </div>
         )
       }

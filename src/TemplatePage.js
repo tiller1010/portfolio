b3be164541c0e1a310b032f79c2d85e3a16df645
Tyler Trout <tiller1010@gmail.com>
2020-08-15 19:19:59 -0400

    Template script

--- src/TemplatePage.js
+++ src/TemplatePage.js
@@ -11,6 +11,96 @@ class TemplatePage extends Component {
 			banner: sessionStorage.getItem('banner')
 		})
 	}
+
+	componentDidMount(){
+
+		//For carousel
+		const carouselImages=document.querySelectorAll('.carouselImage');
+		const track=document.querySelector('.carouselTrack');
+		const slides=Array.from(track.children);
+		const previousButton=document.querySelector('.carouselButtonLeft');
+		const nextButton=document.querySelector('.carouselButtonRight');
+		const navCircles=document.querySelector('.carouselNav');
+		const dots = Array.from(navCircles.children);
+
+
+		//Display all once loaded
+		const displayImages=(image,index)=>{	
+			image.style.display='block';
+		}
+
+		//Arrangement left to right
+		const setSlidePosition=(slide,index)=>{
+			let slideSize=slides[0].getBoundingClientRect();
+			let slideWidth=slideSize.width;
+			slide.style.left=slideWidth*index+'px';
+		}
+
+		//Display images in a row
+		carouselImages.forEach(displayImages);
+		slides.forEach(setSlidePosition);
+
+		const moveToSlide=(track, currentSlide, targetSlide)=>{
+			if(targetSlide){
+				track.style.transform='translateX(-'+targetSlide.style.left+')';
+				currentSlide.classList.remove('currentSlide');
+				targetSlide.classList.add('currentSlide');
+			}
+		}
+
+		const upDateDots=(currentDot, targetDot)=>{
+			if(targetDot){
+				currentDot.classList.remove('currentSlide')
+				targetDot.classList.add('currentSlide')
+			}
+		}
+
+
+		nextButton.addEventListener('click', event=>{
+			const currentSlide=track.querySelector('.currentSlide');
+			const nextSlide=currentSlide.nextElementSibling;
+			const currentDot=navCircles.querySelector('.currentSlide');
+			const nextDot=currentDot.nextElementSibling;
+
+			moveToSlide(track,currentSlide,nextSlide);
+			upDateDots(currentDot, nextDot);
+		});
+
+		previousButton.addEventListener('click', event=>{
+			const currentSlide=track.querySelector('.currentSlide');
+			const previousSlide=currentSlide.previousElementSibling;
+			const currentDot=navCircles.querySelector('.currentSlide');
+			const previousDot=currentDot.previousElementSibling;
+
+			moveToSlide(track,currentSlide,previousSlide);
+			upDateDots(currentDot, previousDot);
+		});
+
+		//Indicators to change slide on click
+		navCircles.addEventListener('click',event=>{
+			const targetDot=event.target.closest('button');
+			if(!targetDot) return;
+			const currentSlide=track.querySelector('.currentSlide');
+			const currentDot=navCircles.querySelector('.currentSlide');
+			const targetIndex=dots.findIndex(dot=> dot===targetDot);
+			const targetSlide=slides[targetIndex];
+
+			moveToSlide(track, currentSlide, targetSlide);
+			upDateDots(currentDot, targetDot);
+		})	
+
+		//Fade Slogan
+		const slogan = document.getElementById('siteDesc');
+		document.addEventListener('scroll',function(){
+			slogan.style.opacity=slogan.getBoundingClientRect().top/100;
+		});
+
+		// Fix carousel on resize
+		window.onresize = function(){
+			slides.forEach(setSlidePosition);
+		}
+
+	}
 	
 	render(){
 
